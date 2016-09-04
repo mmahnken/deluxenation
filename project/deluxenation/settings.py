e15620000989395b0a67344e2d11fdb2f7284a42
mmahnken <mmm25eg@gmail.com>
2016-09-04 12:33:50 -0700

    removing unnecessary import

--- project/deluxenation/settings.py
+++ project/deluxenation/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-from S3 import CallingFormat
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
