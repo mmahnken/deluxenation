554eec8099c563f773c816418d26ab870e59c9ba
mmahnken <mmm25eg@gmail.com>
2016-12-12 22:51:25 -0800

    adding deluxenation's first unittest.

--- project/drawings/tests.py
+++ project/drawings/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+# from drawings.models import Notebook
 
-# Create your tests here.
+
+class HomepageViewTestCase(TestCase):
+    def test_homepage(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+
+
