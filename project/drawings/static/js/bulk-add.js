339bd3d44af88ca1c753021ca4f54e1141f2d310
mmahnken <mmm25eg@gmail.com>
2017-02-04 13:11:57 -0800

    updating overall layout of bulk-add page - still no backend - fixing weird two-way binding problem between globalGroups and drawing specific groups

--- project/drawings/static/js/bulk-add.js
+++ project/drawings/static/js/bulk-add.js
@@ -3,6 +3,7 @@ angular.module("bulkAdd", [])
     .controller('AddController', function ($scope, $http) {
 
         $scope.groups = [];
+        $scope.globalGroups = {};
         $scope.notebooks = [];
         $scope.drawings = [];
 
@@ -14,34 +15,45 @@ angular.module("bulkAdd", [])
                     $scope.groups = JSON.parse(response.data.data);
         });
 
-        $scope.test = function(){
-          $scope.drawings.push($scope.notebook);
-        };
 
 
 
         $scope.handleDirectoryChoice = function (e){
             for (var i=0; i<e.files.length; i++) {
                 var f = e.files[i];
+                var title = f.name.split('.').slice(0, -1).join(' ');
                 var drawing = {
                     data: URL.createObjectURL(f),
-                    fileObj: f
+                    fileObj: f,
+                    groups: {},
+                    shouldSave: true,
+                    title: title,
+                    favorite: false,
                 };
                 $scope.drawings.push(drawing);
                 $scope.$apply();
             }
         };
 
+        $scope.updateNotebook = function (el){
+            el.d.notebook = el.notebook;
+        };
 
-        //
-        // $scope.stockPrice = Math.random() * 1000;
-        //
-        // $scope.buy = function(nshares) {
-        //     $scope.message = nshares + " shares bought!";
-        //     $timeout(function () {
-        //         $scope.message = "";
-        //     }, 1500);
-        // };
+        $scope.updateAllNotebooks = function (el){
+
+            for (var i=0; i < $scope.drawings.length; i++) {
+                var d = $scope.drawings[i];
+                d.notebook = el.globalNotebook;
+            }
+        };
+
+
+        $scope.updateAllGroups = function (el){
+            for (var i=0; i < $scope.drawings.length; i++) {
+                var d = $scope.drawings[i];
+                d.groups[el.group.pk] = $scope.globalGroups[el.group.pk];
+            }
+        };
     })
 
     .config(function($interpolateProvider) {
